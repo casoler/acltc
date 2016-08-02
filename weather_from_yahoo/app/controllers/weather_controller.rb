16536fbb44d8b75ac7af1006b3f5690538f1c9a0
casoler <casoler11@gmail.com>
2016-08-02 18:24:50 -0500

    commit

--- weather_from_yahoo/app/controllers/weather_controller.rb
+++ weather_from_yahoo/app/controllers/weather_controller.rb
@@ -1,8 +1,8 @@
 class WeatherController < ApplicationController
 
   def index
-    @my_city = params[:my_city].titleize if params[:my_city]
-    @my_state = params[:my_state].upcase if params[:my_state]
+    @my_city = params[:my_city]
+    @my_state = params[:my_state]
 
     if @my_city && @my_state
       @city_data = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22#{@my_city}%2C%20#{@my_state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body
