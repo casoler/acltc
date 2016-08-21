535f1dfe5315e43b973da8c02f51affd1449a759
casoler <casoler11@gmail.com>
2016-08-21 09:34:28 -0500

    google maps initial commit

--- chicago-data/app/assets/javascripts/employees.js
+++ chicago-data/app/assets/javascripts/employees.js
@@ -4,6 +4,14 @@ app.controller('myCtrl', function($scope, $http) {
   $http.get("https://data.cityofchicago.org/resource/tt4n-kn4t.json")
   .then(function(response) {
     $scope.employeeData = response.data;
-    console.log($scope.employeeData);
   });
+
+  $scope.sorterFunc = function(employee){
+    return parseInt(employee.employee_annual_salary);
+  };
+
+  $scope.showTitle = function(employee){
+    $scope.showEmployeeTitle = !$scope.showEmployeeTitle;
+
+  };
 });
